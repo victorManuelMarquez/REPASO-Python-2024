7ba678c4b6ca266f86450715cb9cd41ed251b878
victor <victormmla3lm@gmail.com>
2024-02-24 13:34:59 -0300

    Operaciones matemáticas básicas.

---
+++ Operaciones/Operaciones.py
@@ -0,0 +1,6 @@
+print('Suma: 4+2 =', 4+2)
+print('Resta: 23-2 =', 23-2)
+print('Producto: 3*9 =', 3*9)
+print('División: 42/3 =', 42/3)
+print(f'Division: {45}//{7} =', 45//7, '| resto:', 45%7)
+print('Exponenciación: 3^3 =', 3**3)

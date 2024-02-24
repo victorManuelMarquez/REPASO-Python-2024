eb376e7dd8bbf9d7c8d653a2ac524a4488cc7bc9
victor <victormmla3lm@gmail.com>
2024-02-24 14:41:59 -0300

    Bucles.

---
+++ Bucles/Bucles.py
@@ -0,0 +1,12 @@
+cont = 0
+print('Ciclo While:')
+while cont < 10:
+    cont += 1
+    print(cont, end='/')
+print('\nFin del ciclo while.')
+lista = ["Banana", "Naranjas", "Berenjena", "Ananá", "Manzana", "Pera", "Tomate"]
+
+print('Cliclo For:')
+for item in lista:
+    print('\t',item)
+print('Fin del ciclo For')

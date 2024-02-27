7a57a76b90b308370e974f512f3b59f4541c1e4e
Víctor Manuel Márquez <107000454+victorManuelMarquez@users.noreply.github.com>
2024-02-27 19:01:08 -0300

    Argumento Keyword.

--- Funciones/Funciones.py
+++ Funciones/Funciones.py
@@ -8,3 +8,9 @@ def otra_funcion(*valores):
   print(valores)
 
 otra_funcion("Hola", ' ', "Mundo!!!")
+
+# argumento predeterminado
+def funcion_pred(arg = 1000)
+  print('valor recibido :', arg)
+
+funcion_pred()

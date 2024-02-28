21df34c592ed007823c1dadf2cd29931e8ee2665
victor <victormmla3lm@gmail.com>
2024-02-28 16:35:17 -0300

    Funciones con argumentos arbitrarios posicionales.

--- Funciones/Funciones.py
+++ Funciones/Funciones.py
@@ -10,7 +10,13 @@ def otra_funcion(*valores):
 otra_funcion("Hola", ' ', "Mundo!!!")
 
 # argumento predeterminado
-def funcion_pred(arg = 1000)
+def funcion_pred(arg = 1000):
   print('valor recibido :', arg)
 
 funcion_pred()
+
+# argumentos arbitrarios posicionales
+def nueva_funcion(**valores):
+  print(valores)
+  
+nueva_funcion(arg1='Juan Perez', arg2=24, arg3=True)

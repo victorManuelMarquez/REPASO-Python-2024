0ed5840ca766ae2991b7c7989f294a9f86a92e25
Víctor Manuel Márquez <107000454+victorManuelMarquez@users.noreply.github.com>
2024-03-02 22:32:41 -0300

    Listas en python.

---
+++ Listas/Listas.py
@@ -0,0 +1,19 @@
+lista = ['contenido', 12, 7.8, True]
+
+print(lista)
+
+print("bucle for:")
+
+for i in range(len(lista))
+  print(lista[i])
+
+print("bucle while:")
+
+i = 0
+while i in len(lista)
+  print(lista[i])
+  i += 1
+
+print("comprensión:")
+
+[print(valor) for valor in lista]
